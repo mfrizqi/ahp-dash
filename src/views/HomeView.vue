1825da519ab5ca93a7b636661fd628b32b31e0b9
developer_fe_oca <developer@ocatelkom.co.id>
2023-07-04 00:43:42 +0700

    upload file data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,7 @@ import SectionMain from "@/components/SectionMain.vue";
 import CardBoxWidget from "@/components/CardBoxWidget.vue";
 import CardBox from "@/components/CardBox.vue";
 // import TableSampleClients from "@/components/TableSampleClients.vue";
+import FormFilePicker from "@/components/FormFilePicker.vue";
 import TableMahasiswa from "@/components/TableMahasiswa.vue";
 import LayoutAuthenticated from "@/layouts/LayoutAuthenticated.vue";
 import SectionTitleLineWithButton from "@/components/SectionTitleLineWithButton.vue";
@@ -29,22 +30,9 @@ let studentDatas = reactive({
   uploadFinal: [],
 });
 
-const isInit = ref(true);
-
-const readExcelFile2 = async () => {
-  const url = new URL("./data_wisudawan.xls", import.meta.url).href;
-  const data = await (await fetch(url)).arrayBuffer();
-  // console.log("readExcelFile2");
-  // console.log(url);
-  // console.log(data);
-  /* data is an ArrayBuffer */
-  const workbook = XLSX.read(data);
-
-  const firstSheetName = workbook.SheetNames[0];
-  const worksheet = workbook.Sheets[firstSheetName];
-  const sheetValues = XLSX.utils.sheet_to_json(worksheet);
-  studentDatas.raw = sheetValues;
-};
+const form = reactive({
+  file: null,
+});
 
 const readExcelFileLatest = async () => {
   const url = new URL("./data_wisudawan_latest_ext.xls", import.meta.url).href;
@@ -230,77 +218,285 @@ const calculateCompetitionScore = () => {
     };
   });
   studentDatas.raw = calculatedCompetitions;
+  console.log("calculateCompetitionScore");
+  console.log(studentDatas.raw);
 };
 
 //
 // Function: Menghithung AHP dari tiap Mahasiswa
 //
 const calculateStudent = () => {
-  if (isInit.value) {
-    studentDatas.calculated = studentDatas.raw.map((el) => {
-      let ahp = {
-        ipk: 0,
-        tak: 0,
-        prestasi: 0,
-      };
+  // if (isInit.value) {
+  studentDatas.calculated = studentDatas.raw.map((el) => {
+    let ahp = {
+      ipk: 0,
+      tak: 0,
+      prestasi: 0,
+    };
 
-      //Checking GPA / IPK
-      if (el.GPA >= 3) {
-        ahp.ipk = AHPValue.ipk.sub.greater * AHPValue.ipk.main;
-      } else {
-        ahp.ipk = AHPValue.ipk.sub.lesser * AHPValue.ipk.main;
-      }
-
-      // Checking TAK / STUDENT ACTIVITY SCORE
-      if (el.STUDENTACTIVITYSCORE >= 60) {
-        ahp.tak = AHPValue.tak.sub.greater * AHPValue.tak.main;
-      } else {
-        ahp.tak = AHPValue.tak.sub.lesser * AHPValue.tak.main;
-      }
-
-      // Checking Prestasi / SCORE
-      if (el.SCORE >= 5) {
-        ahp.prestasi = AHPValue.prestasi.sub.greater * AHPValue.prestasi.main;
-      } else {
-        ahp.prestasi = AHPValue.prestasi.sub.lesser * AHPValue.prestasi.main;
-      }
-
-      ahp.total = ahp.ipk + ahp.tak + ahp.prestasi;
-
-      return {
-        ...el,
-        ahp,
-        ahpTotal: ahp.total,
-      };
-    });
-
-    // Sort Descending berdasarkan AHP
-    studentDatas.calculated.sort((a, b) => {
-      return b.ahpTotal - a.ahpTotal;
-    });
-
-    // Filter Data Mahasiswa yang nilai AHP > 0.8
-    studentDatas.finalAHP = studentDatas.calculated.filter(
-      (el) => el.ahpTotal > 0.8
-    );
-
-    // Sort data final AHP descending
-    studentDatas.finalAHP.sort((a, b) => {
-      return b.SCORE - a.SCORE;
-    });
-
-    console.log(studentDatas.finalAHP);
-
-    for (let i = 0; i < 5; i++) {
-      studentDatas.sortAHP.push(studentDatas.finalAHP[i]);
+    //Checking GPA / IPK
+    if (el.GPA >= 3) {
+      ahp.ipk = AHPValue.ipk.sub.greater * AHPValue.ipk.main;
+    } else {
+      ahp.ipk = AHPValue.ipk.sub.lesser * AHPValue.ipk.main;
     }
 
-    console.log(studentDatas.sortAHP);
+    // Checking TAK / STUDENT ACTIVITY SCORE
+    if (el.STUDENTACTIVITYSCORE >= 60) {
+      ahp.tak = AHPValue.tak.sub.greater * AHPValue.tak.main;
+    } else {
+      ahp.tak = AHPValue.tak.sub.lesser * AHPValue.tak.main;
+    }
+
+    // Checking Prestasi / SCORE
+    if (el.SCORE >= 5) {
+      ahp.prestasi = AHPValue.prestasi.sub.greater * AHPValue.prestasi.main;
+    } else {
+      ahp.prestasi = AHPValue.prestasi.sub.lesser * AHPValue.prestasi.main;
+    }
+
+    ahp.total = ahp.ipk + ahp.tak + ahp.prestasi;
+
+    return {
+      ...el,
+      ahp,
+      ahpTotal: ahp.total,
+    };
+  });
+
+  // Sort Descending berdasarkan AHP
+  studentDatas.calculated.sort((a, b) => {
+    return b.ahpTotal - a.ahpTotal;
+  });
+
+  // Filter Data Mahasiswa yang nilai AHP > 0.8
+  studentDatas.finalAHP = studentDatas.calculated.filter(
+    (el) => el.ahpTotal > 0.8
+  );
+
+  // Sort data final AHP descending
+  studentDatas.finalAHP.sort((a, b) => {
+    return b.SCORE - a.SCORE;
+  });
+
+  console.log(studentDatas.finalAHP);
+
+  for (let i = 0; i < 5; i++) {
+    studentDatas.sortAHP.push(studentDatas.finalAHP[i]);
+  }
+
+  console.log(studentDatas.sortAHP);
+  // }
+};
+
+const uploadFile = (event) => {
+  // console.log(form);
+  // console.log(form.file);
+  const eventFile = event.target.files ? event.target.files[0] : null;
+  studentDatas.calculated = [];
+  studentDatas.finalAHP = [];
+  if (eventFile) {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(eventFile);
+    reader.onload = (e) => {
+      const arrayBuffer = reader.result;
+      var data = new Uint8Array(arrayBuffer);
+      var arr = new Array();
+      for (var i = 0; i != data.length; ++i)
+        arr[i] = String.fromCharCode(data[i]);
+      var bstr = arr.join("");
+      var workbook = XLSX.read(bstr, { type: "binary" });
+      var first_sheet_name = workbook.SheetNames[1];
+      var worksheet = workbook.Sheets[first_sheet_name];
+      // console.log(XLSX.utils.sheet_to_json(worksheet, { raw: true }));
+      var arraylist = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+      studentDatas.raw = arraylist;
+      console.log("uploadFile");
+      console.log(studentDatas.raw);
+
+      const prepData = [];
+      // Loop setiap mahasiswa
+      studentDatas.raw.forEach((el) => {
+        // console.log(el);
+        // Split setiap baris kompetisi
+        const competitions = el.COMPETITIONLIST.split(/\r?\n/);
+
+        const competitionsFinal = [];
+        const competitionsTitle = [];
+        competitions.forEach((compt) => {
+          let lomba = {
+            title: "",
+            jenis: null,
+            tingkat: null,
+            juara: null,
+          };
+          const sp = compt.split("_");
+          // console.log(sp);
+          lomba.title = sp[0];
+          lomba.value = sp[1];
+
+          if (sp[0] !== "-") {
+            competitionsTitle.push(sp[0]);
+
+            competitionsFinal.push(lomba);
+            if (sp[1]) {
+              // console.log(sp[1]);
+              const competeValue = sp[1].split(",");
+              // console.log(competeValue);
+              if (competeValue) {
+                if (sp[1][0]) {
+                  lomba.jenis = competeValue[0];
+                }
+                if (sp[1][1]) {
+                  lomba.tingkat = competeValue[1];
+                }
+                if (sp[1][2]) {
+                  lomba.juara = competeValue[2];
+                }
+              }
+            }
+          }
+        });
+
+        // console.log(competitionsTitle);
+        // console.log(competitionsFinal);
+        prepData.push({
+          ...el,
+          competitions: competitionsTitle,
+          competitionsFinal: competitionsFinal,
+        });
+      });
+
+      studentDatas.raw = prepData;
+
+      // calculateCompetitionScore();
+
+      // Calculate Competition Upload Score
+      const calculatedCompetitions = studentDatas.raw.map((el) => {
+        let SCORE = 0;
+
+        el.competitionsFinal.forEach((cf) => {
+          console.log(cf);
+          if (cf.jenis !== "rekognisi" && cf.jenis && cf.tingkat && cf.juara) {
+            SCORE += fixedPrestasi[cf.jenis][cf.tingkat][cf.juara];
+          }
+
+          if (cf.jenis === "rekognisi" && cf.jenis && cf.tingkat) {
+            SCORE += fixedPrestasi[cf.jenis][cf.tingkat];
+          }
+        });
+        return {
+          ...el,
+          SCORE: SCORE,
+        };
+      });
+      studentDatas.raw = calculatedCompetitions;
+      console.log("calculateCompetitionScore");
+      console.log(studentDatas.raw);
+
+      // Calculate ahp uploaded file
+      studentDatas.calculated = [];
+      studentDatas.calculated = studentDatas.raw.map((el) => {
+        let ahp = {
+          ipk: 0,
+          tak: 0,
+          prestasi: 0,
+        };
+
+        //Checking GPA / IPK
+        if (el.GPA >= 3) {
+          ahp.ipk = AHPValue.ipk.sub.greater * AHPValue.ipk.main;
+        } else {
+          ahp.ipk = AHPValue.ipk.sub.lesser * AHPValue.ipk.main;
+        }
+
+        // Checking TAK / STUDENT ACTIVITY SCORE
+        if (el.STUDENTACTIVITYSCORE >= 60) {
+          ahp.tak = AHPValue.tak.sub.greater * AHPValue.tak.main;
+        } else {
+          ahp.tak = AHPValue.tak.sub.lesser * AHPValue.tak.main;
+        }
+
+        // Checking Prestasi / SCORE
+        if (el.SCORE >= 5) {
+          ahp.prestasi = AHPValue.prestasi.sub.greater * AHPValue.prestasi.main;
+        } else {
+          ahp.prestasi = AHPValue.prestasi.sub.lesser * AHPValue.prestasi.main;
+        }
+
+        ahp.total = ahp.ipk + ahp.tak + ahp.prestasi;
+
+        return {
+          ...el,
+          ahp,
+          ahpTotal: ahp.total,
+        };
+      });
+
+      console.log("calculated uploaded");
+      console.log(studentDatas.calculated);
+
+      // Sort Descending berdasarkan AHP
+      studentDatas.calculated.sort((a, b) => {
+        return b.ahpTotal - a.ahpTotal;
+      });
+
+      console.log("calculated uploaded sorted");
+      console.log(studentDatas.calculated);
+
+      // Filter Data Mahasiswa yang nilai AHP > 0.8
+      studentDatas.finalAHP = [];
+      studentDatas.finalAHP = studentDatas.calculated.filter(
+        (el) => el.ahpTotal > 0.8
+      );
+
+      console.log("filter data mahasiswa AHP > 0.8");
+      console.log(studentDatas.finalAHP);
+
+      // Sort data final AHP descending
+      studentDatas.finalAHP.sort((a, b) => {
+        return b.SCORE - a.SCORE;
+      });
+
+      console.log(studentDatas.finalAHP);
+      studentDatas.sortAHP = [];
+      console.log(studentDatas.sortAHP);
+      for (let i = 0; i < 5; i++) {
+        studentDatas.sortAHP.push(studentDatas.finalAHP[i]);
+      }
+
+      console.log(studentDatas.sortAHP);
+    };
   }
 };
 
+/*const calculateCompetitionScore = () => {
+  console.log("uploadRaw calculateCompetitionScoreUpload");
+  console.log(studentDatas.uploadRaw);
+  const calculatedCompetitions = studentDatas.uploadRaw.map((el) => {
+    let SCORE = 0;
+
+    el.competitionsFinal.forEach((cf) => {
+      console.log(cf);
+      if (cf.jenis !== "rekognisi" && cf.jenis && cf.tingkat && cf.juara) {
+        SCORE += fixedPrestasi[cf.jenis][cf.tingkat][cf.juara];
+      }
+
+      if (cf.jenis === "rekognisi" && cf.jenis && cf.tingkat) {
+        SCORE += fixedPrestasi[cf.jenis][cf.tingkat];
+      }
+    });
+    return {
+      ...el,
+      SCORE: SCORE,
+    };
+  });
+  studentDatas.uploadRaw = calculatedCompetitions;
+
+  calculateStudentUpload();
+}; */
+
 onMounted(async () => {
-  // await readExcelFile2();
   await readExcelFileLatest();
   calculateCompetitionScore();
   await calculateStudent();
@@ -341,6 +537,14 @@ onMounted(async () => {
           label="Mahasiswa Berprestasi"
         />
       </div>
+
+      <CardBox class="mb-4">
+        <FormFilePicker
+          v-model="form.file"
+          label="Upload"
+          @change="uploadFile($event)"
+        />
+      </CardBox>
 
       <SectionTitleLineWithButton
         :icon="mdiMedal"
